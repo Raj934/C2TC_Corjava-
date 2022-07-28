6d6098183e74e629892e0583df8b718a7a2f9dea
XLBYTE <XLBYTE@DESKTOP-BQEIPUL.alphionsee.in>
2022-07-28 22:57:14 +0530

    JPA

---
+++ JPACrudOperations/src/com/cg/entities/Student.java
@@ -0,0 +1,28 @@
+package com.cg.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
+
+public class Student 
+{
+	private static final long serialVersionUID = 1L;
+	@Id
+	private int studentId;
+	private String name;
+	public int getStudentId() {
+	return studentId;
+	}
+	public void setStudentId(int studentId) {
+	this.studentId = studentId;
+	}
+	public String getName() {
+	return name;
+	}
+	public void setName(String name) {
+	this.name = name;
+	}
+}
